8b2aaef2662a9042920980973727b73759e23e37
yu-teacher <yusm1231@gmail.com>
2025-11-11 01:08:06 +0900

    first commit

---
+++ src/main/java/com/woowa/woowago/WoowagoApplication.java
@@ -0,0 +1,13 @@
+package com.woowa.woowago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowa/woowago/WoowagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowa.woowago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
